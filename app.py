287ba36298f8a4349ec7fae3971a25841d7fd8b3
Ryosuke Inoue <>
2024-07-17 18:47:54 +0900

    GPT追加

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import json
 import pandas as pd
 from datetime import datetime
 import sqlite3
+from openai import OpenAI
+import os
 
 today = datetime.today().strftime('%Y-%m-%d')
 
@@ -52,6 +54,22 @@ if record:
     print(f"コーピングリスト： {record}")
 else:
     print(f"No record found for score_id {score_id}")
+
+
+
+api_key = 'APIKEY'
+
+client = OpenAI(api_key=api_key)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {"role": "system", "content": "あなたは疲れているビジネスマンに休憩の方法をアドバイスする、経験豊富なアドバイザーです。働きすぎている人に警告を出す語調で話してください。"},
+        {"role": "user", "content": "5分程度でできるマインドフルネスのメニューを紹介してください。"}
+    ],
+    model="gpt-3.5-turbo",
+)
+
+print(chat_completion.choices[0].message)
 
 # データベース接続を閉じる
 conn.close()
