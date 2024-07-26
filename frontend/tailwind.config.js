ca1b966f1fdd18294b0ef0b7558a9f1ac9c500bb
masayukikudo <masayuki.kudo0307@gmail.com>
2024-07-26 20:35:53 +0900

    Initial commit to new repository

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
